d73dac48a4e6cdb74e288d8bee4e3f1e267db198
loveandattention <loveandattention@163.com>
2022-05-03 19:14:06 +0800

    Kafka，构建TB级异步消息系统

--- src/main/java/com/nowcoder/community/dao/CommentMapper.java
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -18,4 +18,6 @@ public interface CommentMapper {
 
     int insertComment(Comment comment);
 
+    Comment selectCommentById(int id);
+
 }
--- src/main/java/com/nowcoder/community/service/CommentService.java
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -57,4 +57,8 @@ public class CommentService implements CommunityConstant {
         return rows;
     }
 
+    public Comment findCommentById(int id) {
+        return commentMapper.selectCommentById(id);
+    }
+
 }
