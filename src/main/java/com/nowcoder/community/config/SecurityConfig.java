760fe99f6498e67fa7fccf9595f5ece14c3f5524
loveandattention <loveandattention@163.com>
2022-05-06 20:36:38 +0800

    单元测试、项目监控

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
---
+++ src/test/java/com/nowcoder/community/TransactionTests.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.service.AlphaService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Ma Tengfei
+ * @create 2022-05-06 20:33
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTests {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1() {
+        Object obj = alphaService.save1();
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testSave2() {
+        Object obj = alphaService.save2();
+        System.out.println(obj);
+    }
+
+}
