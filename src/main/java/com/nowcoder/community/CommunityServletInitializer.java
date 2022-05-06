47644f9fc6c49801321e092d6673e6f0d25c5b61
loveandattention <loveandattention@163.com>
2022-05-06 21:25:51 +0800

    项目部署

---
+++ src/main/java/com/nowcoder/community/CommunityServletInitializer.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Ma Tengfei
+ * @create 2022-05-06 20:56
+ */
+public class CommunityServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CommunityApplication.class);
+    }
+}
---
+++ src/main/java/com/nowcoder/community/config/AlphaConfig.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Ma Tengfei
+ * @create 2022-05-06 20:57
+ */
+@Configuration
+public class AlphaConfig {
+
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+}
